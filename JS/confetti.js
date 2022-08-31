29e7c3da8235f40d7eba19b77163f2a5d0071d9b
ismailahmedsh <110427903+ismailahmedsh@users.noreply.github.com>
2022-08-31 11:15:42 +0400

    added section 3: timeline

--- JS/confetti.js
+++ JS/confetti.js
@@ -72,8 +72,8 @@ var removeConfetti; //call to stop the confetti animation and remove all confett
 
 			})();
 		}
-	setTimeout(function(){window.open('./docs/Shehata Resume CVV.pdf')},2750);
-	setTimeout(function(){document.location.reload(true)},3000);
+	setTimeout(function(){window.open('./docs/Shehata Resume CVV.pdf')},2500);
+	setTimeout(function(){document.location.reload(true)},2750);
 	}
 
 	function stopConfettiInner() {
