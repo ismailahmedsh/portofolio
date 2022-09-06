b6447152ba135f23754fd784d879439ed9e4f440
ismailahmedsh <110427903+ismailahmedsh@users.noreply.github.com>
2022-09-06 15:38:09 +0400

    modified section 2 and fixed alot of elements

--- JS/preloader.js
+++ JS/preloader.js
@@ -1,12 +1,11 @@
 
 var loader=document.querySelector(".start-loader");
 var headeranimation=document.querySelector("header");
-loader.classList.add("disappear");
-window.addEventListener("load",vanish)
+
+window.addEventListener("load",vanish);
 
     function vanish(){
-        
-        setTimeout(function() { headeranimation.classList.add("header-after-animation"); },1600);
-        window.scrollTo(0,0);
+        loader.classList.add("disappear");
+        setTimeout(function() { headeranimation.classList.add("header-after-animation"); },2500);
     }
-
+    window.scrollTo(0,0);
