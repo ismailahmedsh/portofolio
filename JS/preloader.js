9c35d297f01df1e3b4fd92a396ede6190281f48b
ismailahmedsh <110427903+ismailahmedsh@users.noreply.github.com>
2022-09-13 17:33:14 +0400

    a small one remaining for the a links

--- JS/preloader.js
+++ JS/preloader.js
@@ -6,6 +6,6 @@ window.addEventListener("load",vanish);
 
     function vanish(){
         loader.classList.add("disappear");
-        setTimeout(function() { headeranimation.classList.add("header-after-animation"); },2500);
+        setTimeout(function() { headeranimation.classList.add("header-after-animation"); },650);
     }
     window.scrollTo(0,0);
